3f2f447c47ff5b1a0846b438d96acdee9b4b3d61
Pete Barnes <pete_barnes@me.com>
2019-03-16 20:51:30 -0400

    Initial Commit

---
+++ CleanStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Pete Barnes on 3/16/19.
+//  Copyright © 2019 Pete Barnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
